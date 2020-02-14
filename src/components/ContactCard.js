f3e889756186ff87bff1acf469ca364ed3e905de
rafagomez92 <rafael_gomez92@hotmail.com>
2020-02-13 23:54:20 -0600

    Se modifico el js de directory para agregar eventos en los botones

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const ContactCard = ({info}) => {
+const ContactCard = (props) => {
     return (        
-        <div className="container">                       
-            <div className="card bg-dark text-white mt-3">
-                <img src={info.imgUrl} width="150" height="250" className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{info.name}</h5>
-                    <p className="card-text">{info.phone}</p>
-                    <p className="card-text">{info.email}</p>
+        <div className="container">
+            <div className="card mt-3 bg-danger text-white text-center" style={{width: '18rem'}}>
+                <img src={props.info.imgUrl} className="card-img-top" alt="..."/>
+                <div className="card-body font-weight-bold">
+                    <p className="card-text">{props.info.name}</p>
+                    <p className="card-text">{props.info.phone}</p>
+                    <p className="card-text">{props.info.email}</p>
                 </div>
             </div>
         </div>
--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,22 +1,50 @@
 import React, { Component } from 'react';
 import contactsData from '../contactsData';
 import ContactCard from './ContactCard';
+import AddContact from './AddContact';
+import RemoveContact from './RemoveContact';
 
 
-class Directory extends Component {
+class Directory extends Component {    
 constructor() {
     super();
     this.state = {
-        contacts: contactsData
+        contacts: []            
     };
+    //bindear el contexto para acceder a this
+    this.addContact = this.addContact.bind(this);
+    this.removeContact = this.removeContact.bind(this);
 }
+
+addContact = () => {
+    this.setState({
+        contacts: contactsData
+    }    
+    );
+}
+
+removeContact = () => {
+    this.setState({
+        contacts: []
+    }    
+    );
+}
+
+
+
+
 render() {
     const contact = this.state.contacts.map((contacto, idx) =>
     <ContactCard info={contacto} key={idx} /> );
-    
+
     return(
         <>
-          {contact}
+        <div className="container">                            
+                    <span className="mr-2"><AddContact add={this.addContact}/></span>
+                    <RemoveContact remove={this.removeContact}/>                                             
+                {contact}
+        </div>
+
         </>
     );
 }
