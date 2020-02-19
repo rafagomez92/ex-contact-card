a59d4c9bcb3a9cae950ceda8039c01ad4481086a
rafagomez92 <rafael_gomez92@hotmail.com>
2020-02-18 21:04:05 -0600

    Agruegue un nuevo metodo para extraer datos del json

--- src/containers/FormContainer.jsx
+++ src/containers/FormContainer.jsx
@@ -2,66 +2,73 @@ import React, { Component } from 'react';
 import ContactCard from '../components/ContactCard';
 import Button from '../components/Button'
 import Input from '../components/Input'
-import contactsData from '../contactsData';
+// import contactsData from '../contactsData';
+import AddContact from '../components/AddContact';
 
 class FormContainer extends Component {
-    constructor() {
-        super();
-        this.state = {
-            newContact: {
-                name:'',
-                phone:'',
-                email:''
-            },
-            contactsSaved: contactsData
-        }
-
-        this.handleName = this.handleName.bind(this);
-        this.handlePhone = this.handlePhone.bind(this);
-        this.handleEmail = this.handleEmail.bind(this);
-        this.handleFormSubmit = this.handleFormSubmit.bind(this);
-        this.handleClearForm = this.handleClearForm.bind(this);    
+    state = {
+        newContact: {
+            name:'',
+            phone:'',
+            email:''
+        },
+        contactsSaved: []        
     }
 
-    // Metodos
-    handleName(e){
-        let value = e.target.value;  //e.target es cuando se clickea el elemento
+    // Metodo generico
 
-        this.setState(
-            prevState => ({
-                newContact: {
-                    ...prevState.newContact, // con este nos sirve para guardar los valores anteriores para no perder los demas datos
-                    name: value //y solo actualizamos el name
-                }
-            })
-        );
-    }
-    handlePhone(e){
+    handleInput = (e) => {
         let value = e.target.value;
-
+        let name = e.target.name;
         this.setState(
             prevState => ({
                 newContact: {
-                    ...prevState.newContact, 
-                    phone: value 
+                    ...prevState.newContact,
+                    [name]:value
                 }
             })
-        );
-    }
-    handleEmail(e){
-        let value = e.target.value;
-
-        this.setState(
-            prevState => ({
-                newContact: {
-                    ...prevState.newContact, 
-                    email: value 
-                }
-            })
-        );
+        )
     }
 
-    handleFormSubmit(e) {
+
+    // handleName = (e) => {
+    //     let value = e.target.value;  //e.target es cuando se clickea el elemento
+
+    //     this.setState(
+    //         prevState => ({
+    //             newContact: {
+    //                 ...prevState.newContact, // con este nos sirve para guardar los valores anteriores para no perder los demas datos
+    //                 name: value //y solo actualizamos el name
+    //             }
+    //         })
+    //     );
+    // }
+    // handlePhone = (e) => {
+    //     let value = e.target.value;
+
+    //     this.setState(
+    //         prevState => ({
+    //             newContact: {
+    //                 ...prevState.newContact, 
+    //                 phone: value 
+    //             }
+    //         })
+    //     );
+    // }
+    // handleEmail = (e) => {
+    //     let value = e.target.value;
+
+    //     this.setState(
+    //         prevState => ({
+    //             newContact: {
+    //                 ...prevState.newContact, 
+    //                 email: value 
+    //             }
+    //         })
+    //     );
+    // }
+
+    handleFormSubmit = (e) => {
         e.preventDefault();
         let userData = this.state.newContact;
         this.setState(prevState => (
@@ -79,7 +86,7 @@ class FormContainer extends Component {
         ));
     }
 
-    handleClearForm(e) {
+    handleClearForm = (e) => {
         e.preventDefault();
         this.setState(            
             {newContact: {
@@ -89,6 +96,31 @@ class FormContainer extends Component {
             }}
         );
     }
+
+    // Este es el metodo que cree para mandarlo a llamar al clickear mi boton de agregar
+    addContact = () => { 
+        const url = 'https://jsonplaceholder.typicode.com/users';
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {                
+                this.setState({
+                    contactsSaved: data
+                });
+            });
+    }        
+
+
+    // componentDidMount() {        
+    //  De esta manera podemos extraer datos de una url y convertirlo a un json
+    //     const url = 'https://jsonplaceholder.typicode.com/users';
+    //     fetch(url)
+    //         .then(response => response.json())
+    //         .then(data => {                
+    //             this.setState({
+    //                 contactsSaved: data
+    //             });
+    //         });
+    // }
 
 
     render() {
@@ -109,21 +141,21 @@ class FormContainer extends Component {
                     type="text"  //se ingresa tipo texto
                     value={this.state.newContact.name}
                     placeholder="Nombre"
-                    handleChange={this.handleName}
+                    handleChange={this.handleInput}
                     />
                     <Input 
                     name="phone"
                     type="number" //solo recibe datos numericos
                     value={this.state.newContact.phone}
                     placeholder="Telefono"
-                    handleChange={this.handlePhone}
+                    handleChange={this.handleInput}
                     />
                     <Input
                     name="email"
                     type="text"
                     value={this.state.newContact.email}
                     placeholder="Correo electrónico"
-                    handleChange={this.handleEmail}
+                    handleChange={this.handleInput}
                     />
                     <Button
                         action={this.handleFormSubmit}                     
@@ -132,8 +164,10 @@ class FormContainer extends Component {
                     <Button
                         action={this.handleClearForm}                     
                         title="Reiniciar"
-                    />
+                    />                    
                 </form>
+                {/* Este es mi botton para agregar aca mando a llamar el metodo addContact() */}
+                <AddContact add={this.addContact}/>
                 </div>
             </div>
         )
